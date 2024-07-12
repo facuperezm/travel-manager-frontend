a4065c5216735eb93c56930ea3b9a21dcdbac0d2
facuperezm <facundoperezmontalvo@gmail.com>
2024-07-12 15:19:24 -0300

    chore: Update dependencies and remove unused code

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,27 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { CreateTripPage } from "./pages/create-trip";
-import { TripDetailsPage } from "./pages/trip-details";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { CreateTripPage } from './pages/create-trip';
+import { TripDetailsPage } from './pages/trip-details';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './lib/react-query';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <CreateTripPage />,
   },
   {
-    path: "/trips/:tripId",
+    path: '/trips/:tripId',
     element: <TripDetailsPage />,
   },
 ]);
 
 export function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="relative h-full w-full bg-background">
+        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_14px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_10%,transparent_100%)]"></div>
+        <RouterProvider router={router} />;
+      </div>
+    </QueryClientProvider>
+  );
 }
--- src/pages/create-trip/index.tsx
+++ src/pages/create-trip/index.tsx
@@ -1,10 +1,29 @@
-import MyButton from '@/components/button';
+import { Button } from '@/components/ui/button';
+import { DatePickerWithRange } from './date-range-picker';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 
 export function CreateTripPage() {
   return (
-    <>
-      <h1>testing create</h1>
-      <MyButton />
-    </>
+    <main className="flex h-screen flex-col items-center justify-center gap-6">
+      <header className="space-y-4">
+        <h1 className="text-center text-2xl">Welcome to travel manager app!</h1>
+        <h2 className="leading-tight">
+          Create a trip by filling out the form below and clicking the "Create
+          Trip" button.
+        </h2>
+      </header>
+      <article>
+        <form className="flex gap-2">
+          <Label htmlFor="where">
+            <Input id="where" placeholder="Where are you going?" />
+          </Label>
+          <Label htmlFor="when">
+            <DatePickerWithRange />
+          </Label>
+          <Button type="submit">Create Trip</Button>
+        </form>
+      </article>
+    </main>
   );
 }
