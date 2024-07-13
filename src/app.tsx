a2248cc9ee7fe8f4c6ac3e40e1b456cc3ff937e6
facuperezm <facundoperezmontalvo@gmail.com>
2024-07-13 16:38:44 -0300

    chore: small changes

--- src/app.tsx
+++ src/app.tsx
@@ -18,10 +18,12 @@ const router = createBrowserRouter([
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="relative h-full w-full bg-background">
-        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_14px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_10%,transparent_100%)]"></div>
-        <RouterProvider router={router} />;
+      <div className="fixed left-0 top-0 -z-10 h-full w-full">
+        <div className="relative h-full w-full bg-background">
+          <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_14px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_30%,transparent_100%)]"></div>
+        </div>
       </div>
+      <RouterProvider router={router} />
     </QueryClientProvider>
   );
 }
--- src/pages/create-trip/index.tsx
+++ src/pages/create-trip/index.tsx
@@ -5,16 +5,16 @@ import { Label } from '@/components/ui/label';
 
 export function CreateTripPage() {
   return (
-    <main className="flex h-screen flex-col items-center justify-center gap-6">
+    <main className="flex h-screen flex-col items-center justify-center gap-6 px-4">
       <header className="space-y-4">
         <h1 className="text-center text-2xl">Welcome to travel manager app!</h1>
-        <h2 className="leading-tight">
+        <h2 className="text-center leading-tight">
           Create a trip by filling out the form below and clicking the "Create
           Trip" button.
         </h2>
       </header>
       <article>
-        <form className="flex gap-2">
+        <form className="flex flex-col gap-2 sm:flex-row">
           <Label htmlFor="where">
             <Input id="where" placeholder="Where are you going?" />
           </Label>
